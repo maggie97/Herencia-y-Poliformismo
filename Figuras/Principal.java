88e743d219a14524c24b7dded5fe8a96991118c7
maggie97 <maggielee97@icloud.com>
2017-02-20 20:52:41 -0600

    Figuras - poliformismo y herencia
    Se añadieron dos clases mas con las que se hace poliformismo al usar la
    misma funcion de calcula area

--- Figuras/Principal.java
+++ Figuras/Principal.java
@@ -10,7 +10,13 @@ public class Principal
    public static void main(String[] args)
    {
        Triangulo t1 = new Triangulo(3, 4 );
+       Cuadrado s1 = new Cuadrado(3);
+       Circulo c1 = new Circulo(3);
        t1.calculaArea();
+       s1.calculaArea();
+       c1.calculaArea();
        System.out.println(" El area del triangulo es = " + t1.accedeArea());
+       System.out.println(" El area del cuadrado es = " + s1.accedeArea());
+       System.out.println(" El area del circulo es = " + c1.accedeArea());
    }
 }
