1b2330b2e89249ccb41c303384357d744ffe8fa4
maggie97 <maggielee97@icloud.com>
2017-02-23 12:01:41 -0600

    23/02/17

--- Figuras/Principal.java
+++ Figuras/Principal.java
@@ -12,18 +12,17 @@ public class Principal
    {
        //Figura [] fig = new Figura[3];
        ArrayList <Figura> fig = new ArrayList <Figura>();
-       Iterator <Figura> aux1 = fig.iterator();
-       Iterator <Figura> aux2 = fig.iterator();
        Triangulo t1 = new Triangulo(3, 4 );
        Cuadrado s1 = new Cuadrado(3);
        Circulo c1 = new Circulo(3);
        fig.add(t1);
        fig.add(s1);
        fig.add(c1);
-       
+       Iterator <Figura> aux1 = fig.iterator();
+       Iterator <Figura> aux2 = fig.iterator();
        while(aux1.hasNext())
        {
-           //Figura a = new aux.Next();
+           //Figura a = new aux1.next();   
            // a.calculaArea();
        }
        for(Figura a: fig)
